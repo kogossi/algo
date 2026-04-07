44e2ebbd1df370f5519e63ce63636d24b8abbae2
kogossi <154993265+kogossi@users.noreply.github.com>
2026-04-07 19:10:21 +0500

    Time: 4 ms (76.50%) | Memory: 19.9 MB (91.48%) - LeetSync

---
+++ 860-design-circular-queue/design-circular-queue.py
@@ -0,0 +1,35 @@
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.q = [0] * k
+        self.capacity = k
+        self.front = 0
+        self.rear = -1
+        self.size = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.rear = (self.rear + 1) % self.capacity
+        self.q[self.rear] = value
+        self.size += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.front = (self.front + 1) % self.capacity
+        self.size -= 1
+        return True
+
+    def Front(self) -> int:
+        return -1 if self.isEmpty() else self.q[self.front]
+
+    def Rear(self) -> int:
+        return -1 if self.isEmpty() else self.q[self.rear]
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.capacity
