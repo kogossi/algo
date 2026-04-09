c2bc8ffb7617e2c56f30ab2f360f90fe65ce356b
kogossi <154993265+kogossi@users.noreply.github.com>
2026-04-10 00:32:05 +0500

    Time: 2 ms (16.92%) | Memory: 19.9 MB (10.89%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,24 @@
+class Solution:
+    def floodFill(self, image, sr, sc, color):
+        start_color = image[sr][sc]
+
+        if start_color == color:
+            return image
+
+        rows, cols = len(image), len(image[0])
+
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols:
+                return
+            if image[r][c] != start_color:
+                return
+
+            image[r][c] = color
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        dfs(sr, sc)
+        return image
